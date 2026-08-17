6e66a844dd05806509a8de63d97f1f158a3255cf
Bhuvan Prashant Pokale <r230323@famt.ac.in>
2026-08-17 23:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-print("Hello from Jenkins!")
-print("Revised file")
-print("Hari Hari bol!")
-print("Hare Krishna")
-print("File is modified")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This application was deployed using Jenkins."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
